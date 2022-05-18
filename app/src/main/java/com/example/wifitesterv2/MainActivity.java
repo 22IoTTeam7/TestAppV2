8922a426e6e20b436cb1840efcddb28e9fb8f489
destiny3912 <destiny3910@gmail.com>
2022-05-18 16:19:25 +0900

    FIX : Scanning error

--- app/src/main/java/com/example/wifitesterv2/MainActivity.java
+++ app/src/main/java/com/example/wifitesterv2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.wifitesterv2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -29,23 +31,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            if(wifiStartFlag == true){
-                //TODO [와이파이 스캔이 실행 중인 경우]
-                Log.d("---","---");
-                Log.e("//===========//","================================================");
-                Log.d("","\n"+"[A_WifiScan > 실시간 와이파이 스캐닝이 이미 동작 중입니다 ...]");
-                Log.e("//===========//","================================================");
-                Log.d("---","---");
+        Log.d("permission","권한 요청");
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.CHANGE_WIFI_STATE,
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        Manifest.permission.CHANGE_WIFI_STATE,
+                        Manifest.permission.READ_PHONE_STATE,
+                        Manifest.permission.CHANGE_NETWORK_STATE,
+                        Manifest.permission. ACCESS_COARSE_LOCATION},
+                1000);
+
+            try {
+                if(wifiStartFlag == true){
+                    //TODO [와이파이 스캔이 실행 중인 경우]
+                    Log.d("---","---");
+                    Log.e("//===========//","================================================");
+                    Log.d("","\n"+"[A_WifiScan > 실시간 와이파이 스캐닝이 이미 동작 중입니다 ...]");
+                    Log.e("//===========//","================================================");
+                    Log.d("---","---");
+                }
+                else {
+                    //TODO [와이파이 스캔 시작]
+                    WifiScanStart();
+                }
             }
-            else {
-                //TODO [와이파이 스캔 시작]
-                WifiScanStart();
+            catch (Exception e){
+                e.printStackTrace();
             }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
 
     }
 
@@ -68,8 +83,8 @@ public class MainActivity extends AppCompatActivity {
 
             //TODO [와이파이 스캔 상태 확인]
             boolean success = wifiManager.startScan();
-            if (!success) {
-                Log.d("---","---");
+            Log.d("---","---");
+            if(!success) {
                 Log.e("//===========//","================================================");
                 Log.d("","\n"+"[A_WifiScan > WifiScanStart() 메소드 : 실시간 와이파이 스캐닝 시작 할 수없는 상태]");
                 Log.d("","\n"+"[로직 : 와이파이 스캔 기능이 정지 상태입니다. 와이파이 설정에서 비활성 후 다시 활성 필요]");
@@ -84,7 +99,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             else {
-                Log.d("---","---");
                 Log.w("//===========//","================================================");
                 Log.d("","\n"+"[A_WifiScan > WifiScanStart() 메소드 : 실시간 와이파이 스캐닝 진행 중인 상태]");
                 Log.w("//===========//","================================================");
@@ -127,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
                     wifiList = wifiManager.getScanResults();
                     String ssid_format = "";
                     String mac_format = "";
+                    int level_format;
 
                     //TODO [for 반복문을 수행하면서 데이터 확인 실시]
                     for(int i=0; i<wifiList.size(); i++){
@@ -138,10 +153,13 @@ public class MainActivity extends AppCompatActivity {
                         mac_format = String.valueOf(wifiList.get(i).BSSID.trim());
                         //mac_format = mac_format.replaceAll(":",""); //구분자 제거 코드
 
+                        level_format = Integer.valueOf(wifiList.get(i).level);
+
                         //TODO [JSON 형식으로 포맷 실시]
                         JSONObject jsonObject = new JSONObject();
                         jsonObject.put("SSID", ssid_format);
                         jsonObject.put("MAC",mac_format);
+                        jsonObject.put("Level",level_format);
 
                         //TODO [배열에 데이터 저장 실시]
                         if(wifiFormatList.contains(jsonObject.toString()) == false){ //TODO 중복 저장하지 않음
@@ -156,21 +174,19 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("","\n"+"스캔 개수 : "+String.valueOf(wifiFormatList.size())+"");
                     Log.d("","\n"+"스캔 데이터 : "+String.valueOf(wifiFormatList.toString())+"");
                     Log.w("//===========//","================================================");
-                    Log.d("---","---");
 
                     //TODO [실시간 와이파이 스캐닝 종료]
-                    WifiScanStop();
 
                 } else {
                     Log.d("---","---");
                     Log.e("//===========//","================================================");
                     Log.d("","\n"+"[A_WifiScan > onReceive() 메소드 : 실시간 와이파이 스캐닝 목록 확인 실패]");
                     Log.e("//===========//","================================================");
-                    Log.d("---","---");
 
                     //TODO [실시간 와이파이 스캐닝 종료]
-                    WifiScanStop();
                 }
+                Log.d("---","---");
+                WifiScanStop();
             }
             catch (Exception e){
                 e.printStackTrace();
